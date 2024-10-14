6fc9c1a180f991ef810b25c2549fcb6c05bc02dd
Fabrício Zago Horvat <128423544+fabricio2905@users.noreply.github.com>
2024-10-14 17:42:23 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,26 @@
+// Banco de dados
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.db'); 
+
+db.serialize(() => {
+    // Tabela de usuários
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    )`);
+
+    // Tabela de processos 
+    db.run(`CREATE TABLE IF NOT EXISTS processos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        pid TEXT,
+        usoCpu INTEGER,
+        usoMemoria INTEGER,
+        disco INTEGER,
+        prioridade TEXT,
+        estado TEXT,
+        usuario TEXT
+    )`);
+});
+
+module.exports = db;
